c44901f430456ca806f83742a0305564af13880a
ziad72608 <zm735358@gmail.com>
2023-03-27 17:00:20 +0200

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-char a;
-for (a = 'a' ; a <= 'z' ; a++)
-{
-   putchar(a);
-}
-putchar('\n');
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 return (0);
 }
